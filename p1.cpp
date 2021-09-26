5b7376fcabc941f9ca9670464f1bf4312d175c5a
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-09-26 14:49:14 +0900

    p1.cpp

---
+++ p1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> ret;
+    for(int i = 0; i < nums.size(); i++){
+        for(int j = i + 1; j < nums.size(); j++){
+            if(nums[i] + nums[j] == target) return {i, j};
+        }
+    }
+    return {-1};
+}
+int main(){
+
+}
