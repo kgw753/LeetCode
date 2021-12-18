75b0bc0f01a1fb1a5986a8fc1b8201368bd18b8c
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-12-18 20:53:18 +0900

    p387

---
+++ p387.java
@@ -0,0 +1,15 @@
+public class p387 {
+    public int firstUniqChar(String s) {
+        int[] chars = new int['z' - 'a' + 1];
+
+        for(char c : s.toCharArray()){
+            chars[c - 'a']++;
+        }
+
+        for(int i = 0; i < s.length(); i++){
+            if(chars[s.charAt(i) - 'a'] == 1) return i;
+        }
+
+        return -1;
+    }    
+}
