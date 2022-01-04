86f7cb4aae9735cc11f4d2f162e9b2500325874e
kgw753 <49424965+kgw753@users.noreply.github.com>
2022-01-04 13:27:34 +0900

    p102

--- p102.java
+++ p102.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 class TreeNode {
     int val;
@@ -16,23 +14,23 @@ class TreeNode {
     }
 }
 
+
 public class p102 {
     public static List<List<Integer>> ret;
-
     public List<List<Integer>> levelOrder(TreeNode root) {
         ret = new ArrayList<>();
-
         go(root, 0);
-
         return ret;
-    }    
-    public static void go(TreeNode node, int level){
-        if(node == null) return;
-        if(ret.size() < level + 1) ret.add(new ArrayList<>());
-        
-        ret.get(level).add(node.val);
+    }
 
-        if(node.left != null) go(node.left, level + 1);
-        if(node.right != null) go(node.right, level + 1);
+    public static void go(TreeNode node, int depth){
+        if(node == null) return;
+        if(ret.size() == depth){
+            ret.add(new ArrayList<>());
+        }
+        ret.get(depth).add(node.val);
+
+        go(node.left, depth + 1);
+        go(node.right, depth + 1);
     }
 }
