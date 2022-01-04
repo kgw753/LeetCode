1aebab40785de70cffc8f2bd1c059e8474fa2c03
kgw753 <49424965+kgw753@users.noreply.github.com>
2022-01-04 14:46:43 +0900

    p696

--- p696.java
+++ p696.java
@@ -1,21 +1,19 @@
 public class p696 {
     public int countBinarySubstrings(String s) {
         int ret = 0;
-
-        int now = 1;
         int prev = 0;
-        
-        // 00110011
+        int now = 1;
         for(int i = 1; i < s.length(); i++){
             if(s.charAt(i - 1) != s.charAt(i)){
-                ret += Math.min(now, prev);
+                ret += Math.min(prev, now);
                 prev = now;
                 now = 1;
-            }else{
+            }
+            else{
                 now++;
             }
         }
 
-        return ret + Math.min(now, prev);
+        return ret + Math.min(prev, now);
     }
 }
