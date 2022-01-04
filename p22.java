05be4323343b83906f3251b07a70fa4adc36ad9d
kgw753 <49424965+kgw753@users.noreply.github.com>
2022-01-04 14:00:58 +0900

    p22

--- p22.java
+++ p22.java
@@ -5,23 +5,23 @@ public class p22 {
     public static List<String> ret;
     public List<String> generateParenthesis(int n) {
         ret = new ArrayList<>();
-
-        go(0, 0, "", n);
+        
+        go("", 0, 0, n);
 
         return ret;
     }
 
-    public static void go(int l, int r, String str, int size){
+    public static void go(String str, int l, int r, int size){
         if(l == size && r == size){
             ret.add(str);
             return;
         }
 
-        if(l <= size) {
-            go(l + 1, r, str + "(", size);
-            if(l > r){
-                go(l, r + 1, str + ")", size);    
-            }    
+        if(l < size){
+            go(str + "(", l + 1, r, size);
+        }
+        if(r < l){
+            go(str + ")", l, r + 1, size);
         }
     }
 }
