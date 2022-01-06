1d115a7c64273ccf0476779c5d268cfc765f4aba
kgw753 <49424965+kgw753@users.noreply.github.com>
2022-01-06 18:37:34 +0900

    p3

--- p3.java
+++ p3.java
@@ -2,27 +2,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class p3 {
-    public Map<Character, Integer> mp = new HashMap<>();
     public int lengthOfLongestSubstring(String s) {
-        int ret = 0;
+        int mx = 0;
         int startIdx = 0;
+        Map<Character, Integer> mp = new HashMap<>();
 
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
-            
+
             if(mp.containsKey(c)){
                 startIdx = Math.max(startIdx, mp.get(c) + 1);
             }
-            
+
             mp.put(c, i);
-            ret = Math.max(ret, i - startIdx + 1);
+            mx = Math.max(mx, startIdx - i + 1);
         }
 
-        return ret;
-    }   
-
-    public static void main(String[] args){
-        Solution sol = new Solution();
-        System.out.println(sol.lengthOfLongestSubstring("bbbbb"));
+        return mx;
     }
 }
