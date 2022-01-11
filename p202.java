633077213e094bf737c6d57f5cb87c49e5e5cc6f
kgw753 <49424965+kgw753@users.noreply.github.com>
2022-01-11 13:46:44 +0900

    p202

--- p202.java
+++ p202.java
@@ -2,21 +2,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class p202 {
-    public static Set<Integer> visited = new HashSet<>();
-
+    public static Set<Integer> visited;
     public boolean isHappy(int n) {
+        visited = new HashSet<>();
+        return happyCheck(n);
+    }
+
+    public static boolean happyCheck(int n){
         if(n == 1) return true;
         else if(visited.contains(n)) return false;
-        
+
         visited.add(n);
 
         int next = 0;
 
         while(n > 0){
-            next += (n % 10) * (n % 10);
+            next += Math.pow(n % 10, 2);
             n /= 10;
         }
 
-        return isHappy(next);
+        return happyCheck(next);
     }
 }
